6ac460050a81c27475a20ac57c4618a0a295dcf0
Ryo535 <travismister46@gmail.com>
2025-08-06 12:20:58 +1200

    Test and fix
    I tested the program and found that choicebox didn't allow the user to enter anything they wanted, so I replaced it with enterbox which does allow the user to enter anything they want.

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from easygui import *
 # import everything from the easygui library, to allow me to create guis
 
 # replaced text-based functions with easygui functions
-adjectives = choicebox("2 adjectves: ").split() # storing two adjectives in a list
-verb1 = choicebox("Verb 1: ")
-noun1 = choicebox("Noun 1: ")
+adjectives = enterbox("2 adjectves: ").split() # storing two adjectives in a list
+verb1 = enterbox("Verb 1: ")
+noun1 = enterbox("Noun 1: ")
 
 # using an f string to easily place variables into the story
 msgbox(f"The {adjectives[0]} {noun1} was {verb1} in a very {adjectives[1]} manner.")
