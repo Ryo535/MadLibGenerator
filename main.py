f51705d795aed1070bb0d77ef422dffede2a3a35
Ryo535 <travismister46@gmail.com>
2025-08-08 08:45:26 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from easygui import *
 # import everything from the easygui library, to allow me to create guis
 
-# replaced text-based functions with easygui functions
-adjectives = enterbox("2 adjectves: ").split() # storing two adjectives in a list
-verb1 = enterbox("Verb 1: ")
-noun1 = enterbox("Noun 1: ")
+# changed input descriptions to be questions, leading to more unexpected results.
+color = enterbox("What is your favourite colour?")
+adjectives = enterbox("What are two funny adjectives? Seperate them with a space.").split()
+verb = enterbox("What is your favourite activity?")
+animal = enterbox("What is your favourite animal?")
 
-# using an f string to easily place variables into the story
-msgbox(f"The {adjectives[0]} {noun1} was {verb1} in a very {adjectives[1]} manner.")
+# adjusted story to make use of the new variables (the story is still just a 
+# placeholder being used for testing)
+msgbox(f"The {adjectives[0]} {color} {animal} was {verb} in a very {adjectives[1]} manner.")
