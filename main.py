fada38a6c84787b0b970e747a4c05976790c9edb
Ryo535 <travismister46@gmail.com>
2025-08-11 12:52:14 +1200

    Improved functions, variables and comments

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from easygui import *
-# import everything from the easygui library, to allow me to create guis
+# easygui library is used to create all the guis
+import sys
+# sys library is used to exit the application when user clicks cancel
 
 # function to check if a string is an integer
 def str_to_int(text: str):
     if text.isdigit():
-        return int(text) # returns with the integer value of the string
-    else:
-        return False # returns false if the string is not a valid integer
+        return int(text)
+    return False
 
 # function to check if an integer is within a variable range
-def is_in_range(num: int, start: int, end: int):
+def is_in_range(num, start: int, end: int, loop = False):
+    if loop: 
+        for i in num: 
+            if not (i >= start and i <= end):
+                return False # returns false if ANY of the integers are not within range
+        return True
+    
     if num >= start and num <= end:
-        return True # returns true if the integer is within the range
-    else:
-        return False # returns false if the integer is not within the range
+        return True
+    return False
+
+# function to check if user clicked cancel
+def check_cancel(button):
+    if button == None:
+        sys.exit(0) # closes the program
+    return button
 
 # asking questions to gather user input, making the story unique to each user.
 color = enterbox("What is your favourite colour?")
@@ -27,22 +39,22 @@ while len(adjectives) != 2:
 verb = enterbox("What is your favourite activity?")
 animal = enterbox("What is your favourite animal?")
 
-randomNum = ""
+rand_num = ""
 # loop that ensures the script only continues if the input is a valid integer, preventing errors from occuring.
-while not randomNum:
-    randomNum = str_to_int(enterbox("Pick a number from 1-100"))
-    if not is_in_range(randomNum, 1, 100): randomNum = False
-    if not randomNum: msgbox("Please enter a valid number!")
+while not rand_num:
+    rand_num = str_to_int(enterbox("Pick a number from 1-100"))
+    if not is_in_range(rand_num, 1, 100): rand_num = False
+    if not rand_num: msgbox("Please enter a valid number!")
 
 # placeholder story used for testing
 msgbox(f"The {adjectives[0]} {color} {animal} was {verb} in a very {adjectives[1]} manner.")
 
 # shows a different story depending on the user's chosen number
-if is_in_range(randomNum, 1, 25):
+if is_in_range(rand_num, 1, 25):
     msgbox("story 1")
-elif is_in_range(randomNum, 26, 50):
+elif is_in_range(rand_num, 26, 50):
     msgbox("story 2")
-elif is_in_range(randomNum, 51, 75):
+elif is_in_range(rand_num, 51, 75):
     msgbox("story 3")
 else:
     msgbox("story 4")
