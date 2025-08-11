2c21054331483ca63f6daf6821f5b768d7c15ab0
Ryo535 <travismister46@gmail.com>
2025-08-11 12:54:59 +1200

    Added validity checks to questions

--- main.py
+++ main.py
@@ -28,16 +28,38 @@ def check_cancel(button):
     return button
 
 # asking questions to gather user input, making the story unique to each user.
-color = enterbox("What is your favourite colour?")
+question = "What is your favourite colour?"
+valid = False
+while not valid:
+    color = check_cancel(enterbox(question))
+    valid = is_in_range(len(color), 3, 30)
+    if not valid: msgbox("Please enter a valid colour! It must be between 3-30 characters.")
 
+question = "What are two words you would use to describe your best friend? Seperate them with a space."
 adjectives = []
-# loop that ensures the script only continues if there are exactly two adjectives, preventing errors from occuring.
-while len(adjectives) != 2:
-    adjectives = enterbox("What are two funny adjectives? Seperate them with a space.").split()
-    if len(adjectives) != 2: msgbox("Please enter two adjectives! Remember to seperate them with a space!")
+valid = False
+# loop that ensures the script only continues if there are exactly two valid adjectives, preventing errors from occuring.
+while len(adjectives) != 2 or not valid:
+    adjectives = check_cancel(enterbox(question)).split()
 
-verb = enterbox("What is your favourite activity?")
-animal = enterbox("What is your favourite animal?")
+    valid = is_in_range([len(word) for word in adjectives], 3, 27, True)
+    
+    if len(adjectives) != 2: msgbox("Please enter two adjectives! Remember to seperate them with a space!")
+    elif not valid: msgbox("Please enter valid english adjectives! They each must be between 3-45 characters!")
+
+question = "What is your favourite activity?"
+valid = False
+while not valid:
+    verb = check_cancel(enterbox(question))
+    valid = is_in_range(len(verb), 3, 30)
+    if not valid: msgbox("Please enter a valid verb! It must be between 3-30 characters.")
+
+question = "What is your favourite animal?"
+valid = False
+while not valid:
+    animal = check_cancel(enterbox(question))
+    valid = is_in_range(len(verb), 3, 42)
+    if not valid: msgbox("Please enter a valid animal! It must be between 3-42 characters.")
 
 rand_num = ""
 # loop that ensures the script only continues if the input is a valid integer, preventing errors from occuring.
